e9b7cf6525f9520106c41008e2150b78b2704d42
Miru <116446791+ShedMiru@users.noreply.github.com>
2025-01-21 18:40:06 +0700

    kartu gas desc change

--- components/kartuGas.jsx
+++ components/kartuGas.jsx
@@ -4,20 +4,16 @@ import Image from "next/image";
 const KartuGas = ({ percentage }) => {
     return (
         <div className="relative w-30 h-40 sm:w-40 sm:h-52 border-4 border-[#355F2E] rounded-3xl overflow-hidden">
-            {/* Green Background */}
+            {/* Back Background */}
             <div className="absolute inset-0 bg-[#A9E06D]"></div>
-            {/* Orange Background (Progress) */}
+            {/* Front Background (Progress) */}
             <div
                 className="absolute bottom-0 left-0 w-full bg-[#5CB338]"
                 style={{ height: `${percentage}%` }} // Dynamic height based on percentage
             ></div>
-
-            {/* Content */}
+            {/* Displays */}
             <div className="relative flex flex-col items-center justify-center h-full">
-                {/* Percentage Text */}
                 <p className="text-2xl font-bold text-black z-10">{percentage}%</p>
-
-                {/* Image */}
                 <div className="flex items-center justify-center size-[60%] sm:size-[65%]">
                     <Image
                         aria-hidden
