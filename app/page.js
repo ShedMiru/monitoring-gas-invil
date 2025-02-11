907192f9eeafd2989dad3b0dfd44a75660f65aa4
Miru <116446791+ShedMiru@users.noreply.github.com>
2025-02-11 16:06:19 +0700

    page reset

--- app/page.js
+++ app/page.js
@@ -2,6 +2,24 @@ import Contents from "@/components/contents";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Halo, ini halaman utama!</h1>;
+  return (
+    <div className="bg-[#355F2E] grid grid-rows-[90px_1fr] gap-2 items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <header>
+        <div className="flex items-center gap-4 font-semibold text-6xl">
+          <Image
+            aria-hidden
+            src="/logo.png"
+            alt="Gas icon"
+            width={70}
+            height={79.5}
+          />
+          <h1 className="text-black">
+            S-Bio-S
+          </h1>
+        </div>
+      </header>
+      <Contents
+      />
+    </div>
+  );
 }
-
